37928f9bab3c458409450cb3eca0720734b9b919
blwatkins <blwatkins@users.noreply.github.com>
2026-08-13 14:38:49 -0500

    Update copyright headers across whole project.

--- graph/scale.js
+++ graph/scale.js
@@ -1,14 +1,20 @@
-/* 
-This code and its output are licensed under the
-Creative Commons Attribution-NonCommercial-NoDerivatives 
-4.0 International (CC BY-NC-ND 4.0) License.
-https://creativecommons.org/licenses/by-nc-nd/4.0/
-*/
-
-// Gradient Graphs
-// Scale Class
-// Author: Brittni Watkins
-// Created: March 29, 2022
+/*
+ * Copyright (c) 2022-2026 Brittni Watkins.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 class Scale {
     constructor(min, max) {
